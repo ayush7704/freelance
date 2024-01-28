8e05cd263d451e6dd03a921ce7313ff00af93cb4
ayush nagar <nagarayush570@gmail.com>
2024-01-28 15:04:54 -0800

    first commit

--- script.js
+++ script.js
@@ -1,54 +1,62 @@
-let topbarheight = document.querySelector('.topbar').offsetHeight;
-let headerHeight = document.querySelector('header').offsetHeight
-window.addEventListener('resize', () => {
-    let headerHeight = document.querySelector('header').offsetHeight
+function cssvars() {
+    let headerHeight = document.querySelector('header').offsetHeight;
     let topbarheight = document.querySelector('.topbar').offsetHeight;
+    let page3 = document.querySelector('.page3').offsetHeight
     document.documentElement.style.setProperty('--headerHeight', `${headerHeight}px`)
     document.documentElement.style.setProperty('--topbarheight', `${topbarheight}px`)
-})
-window.addEventListener('DOMContentLoaded', () => {
+    document.documentElement.style.setProperty
+    ('--page3height', `${page3}px`)
+    console.log(headerHeight)
+}
+window.addEventListener('resize', cssvars)
+window.addEventListener('DOMContentLoaded', cssvars)
+cssvars()
 
-    let headerHeight = document.querySelector('header').offsetHeight
-    let topbarheight = document.querySelector('.topbar').offsetHeight;
-    document.documentElement.style.setProperty('--headerHeight', `${headerHeight}px`)
-    document.documentElement.style.setProperty('--topbarheight', `${topbarheight}px`)
-})
-
-
-console.log(headerHeight - topbarheight)
 let ham = document.querySelector('.hamburgue')
-ham.addEventListener('click', function () {
+// ham.addEventListener('click', function () {
 
-    // const block = document.querySelector('.d-none')
-    const hamspan = document.querySelector('.hamburgue span')
+//     // const block = document.querySelector('.d-none')
+//     const hamspan = document.querySelector('.hamburgue span')
 
-    const spanbfr = window.getComputedStyle(hamspan, '::before')
-    const header = document.querySelector('.header')
-    console.log(spanbfr.top)
-    if (spanbfr.top == '-8px') {
-        hamspan.style.background = 'transparent'
-        header.style.display = 'block'
+//     const spanbfr = window.getComputedStyle(hamspan, '::before')
+//     const header = document.querySelector('.header')
+//     console.log(spanbfr.top)
+//     if (spanbfr.top == '-8px') {
+//         hamspan.style.background = 'transparent'
+//         header.style.display = 'block'
 
-        // // gsap point 
-        // gsap.from('.d-none', {
-        //     x: -150,
-        //     opacity: 0,
-        //     duration: 0.7,
-        // })
-        console.log('iff')
-        document.documentElement.style.setProperty('--top', `0px`)
-        document.documentElement.style.setProperty('--bottom', `0px`)
-        document.documentElement.style.setProperty('--rotatebfr', `135deg`)
-        document.documentElement.style.setProperty('--rotateaft', `-135deg`)
-    }
-    else {
-        console.log('else')
-        hamspan.style.background = ''
-        header.style.display = ''
-        document.documentElement.style.setProperty('--top', `-8px`)
-        document.documentElement.style.setProperty('--bottom', `-8px`)
-        document.documentElement.style.setProperty('--rotatebfr', `0deg`)
-        document.documentElement.style.setProperty('--rotateaft', `0deg`)
-    }
+//         // // gsap point 
+//         // gsap.from('.d-none', {
+//         //     x: -150,
+//         //     opacity: 0,
+//         //     duration: 0.7,
+//         // })
+//         console.log('iff')
+//         document.documentElement.style.setProperty('--top', `0px`)
+//         document.documentElement.style.setProperty('--bottom', `0px`)
+//         document.documentElement.style.setProperty('--rotatebfr', `135deg`)
+//         document.documentElement.style.setProperty('--rotateaft', `-135deg`)
+//     }
+//     else {
+//         console.log('else')
+//         hamspan.style.background = ''
+//         header.style.display = ''
+//         document.documentElement.style.setProperty('--top', `-8px`)
+//         document.documentElement.style.setProperty('--bottom', `-8px`)
+//         document.documentElement.style.setProperty('--rotatebfr', `0deg`)
+//         document.documentElement.style.setProperty('--rotateaft', `0deg`)
+//     }
 
-})
+// })
+
+// const splide = new Splide( '.splide', {
+//     type   : 'loop',
+//     drag   : 'free',
+//     focus  : 'center',
+//     perPage: 3,
+//     autoScroll: {
+//       speed: 1,
+//     },
+//   } );
+  
+//   splide.mount();
